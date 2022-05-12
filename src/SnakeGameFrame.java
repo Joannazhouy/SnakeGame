6ec49084d7f169ff766fa4c474cc58c586f6aaba
Joannazhouy <joannazhou.ubc@gmail.com>
2022-05-11 18:33:13 -0700

    creating first class

--- src/SnakeGameFrame.java
+++ src/SnakeGameFrame.java
@@ -1,3 +1,16 @@
-public class SnakeGameFrame {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+public class SnakeGameFrame extends JPanel implements ActionListener {
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
